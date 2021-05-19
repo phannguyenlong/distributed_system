67edebcd62fbd0772fca711348a1bdac01a0f0bd
Long <phannguyenlong0812@gmail.com>
2021-05-19 14:36:13 +0700

    update multithread server

--- sample/socket/multithread/Server.java
+++ sample/socket/multithread/Server.java
@@ -1,20 +1,11 @@
 package sample.socket.multithread;
 import java.net.*;
 public class Server {
-	 private int port = 9999;
-	 ServerSocket socket = null;
-	 
-	 public Server() throws Exception {
-		 this.socket = new ServerSocket(port);
-		 System.out.println("Connect to: " + port);
-	 }
-	 
-	 public ServerSocket getSocket() {
-	        return socket;
-	 }
 	   public static void main(String[] args) {
-	        try {
-	            ServerSocket socket = (new Server()).getSocket();
+			try {
+				int port = 9999;
+				ServerSocket socket = new ServerSocket(port);
+				System.out.println("Server run on port " + port);
 	            while (true) {
 	                Socket client = socket.accept();
 	                
